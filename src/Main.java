b35f83dedb05dc20293fb344f2590ff8c57d48d4
Misael Taperia <askedmisael2@gmail.com>
2025-03-15 03:47:39 -0500

    set up window

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.awt.*;
 public class Main extends JFrame {
     Container con = getContentPane();
     public static void main(String[] MisaMisa) {
-
+        Main fr = new Main();
+        fr.setSize(500,500);
+        fr.setVisible(true);
     }
 
 }
