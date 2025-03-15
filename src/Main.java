f9e5f0fcaacc9a065fab222401e8195c8e818b45
Misael Taperia <askedmisael2@gmail.com>
2025-03-15 03:22:31 -0500

    changed arbitrary args to MisaMisa in line 5

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 public class Main extends JFrame {
     Container con = getContentPane();
-    public static void main(String[] devMisa) {
+    public static void main(String[] MisaMisa) {
 
     }
 
